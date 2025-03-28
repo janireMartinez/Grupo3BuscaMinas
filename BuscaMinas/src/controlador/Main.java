5b5a455982435f0c3dd0bde3c8f297337a2fce97
ikasle <ikasle@DAM1-03.arangoya.local>
2025-03-28 12:24:58 +0100

    Versión: 1.0.1
    Implementación de MVC, con sus respectivas clases.
    
    CLASES:
    - VentanaInicio
    - VentanaJuego
    - VentanaRanking
    - Buscaminas
    - Main (agregado el cambio de ventanas)

---
+++ BuscaMinas/src/controlador/Main.java
@@ -0,0 +1,45 @@
+package controlador;
+
+import vista.VentanaInicio;
+import vista.VentanaJuego;
+import vista.VentanaRanking;
+
+public class Main {
+	
+	private static VentanaInicio ventanaInicio;
+	private static VentanaJuego ventanaJuego;
+	private static VentanaRanking ventanaRanking;
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ventanaInicio = new VentanaInicio();
+		ventanaInicio.setVisible(true);
+
+		
+	}
+	// pasar de la inicial a juego
+	public static void cambioInicioJuego() {
+		ventanaJuego = new VentanaJuego(); // pasar por parametro todos las variables diferente que haya
+		ventanaJuego.setVisible(true);
+		ventanaInicio.setVisible(false);
+	}
+	
+	// pasar de la venta juego a ranking
+	public static void cambioJuegoRanking() {
+		ventanaRanking = new VentanaRanking(); // pasar por parametro todos las variables diferente que haya
+		ventanaRanking.setVisible(true);
+		ventanaJuego.setVisible(false);
+	}
+	
+	// pasar de la venta ranking a inicio
+		public static void cambioRankingInico() {
+			ventanaInicio = new VentanaInicio(); // pasar por parametro todos las variables diferente que haya
+			ventanaInicio.setVisible(true);
+			ventanaRanking.setVisible(false);
+		}
+	
+
+}
