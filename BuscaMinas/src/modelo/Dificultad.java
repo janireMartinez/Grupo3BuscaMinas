cb81f0410d81d55759c828c9328c91ad33febb06
ikasle <ikasle@DAM1-02.arangoya.local>
2025-04-04 12:25:58 +0200

    Version: 0.0.8
    Cambios:
    - Implementacion de la clase enumerada Dificultad.

--- BuscaMinas/src/modelo/Dificultad.java
+++ BuscaMinas/src/modelo/Dificultad.java
@@ -2,6 +2,33 @@ package modelo;
 
 public enum Dificultad {
 	// facil, media, dificil (ATRIBUTOS: filas, columnas, bombas)  y para llamar y tal, static Dificultad obtenerDificultad(String nombre)
+	FACIL(8, 8, 10),
+	
+	MEDIA(16, 16, 40),
+	
+	DIFICIL(16, 30, 99);
+	
+	private int filas;
+	private int columnas;
+	private int bombas;
+	
+	Dificultad(int filas, int columnas, int bombas) {
+		this.filas=filas;
+		this.columnas=columnas;
+		this.bombas=bombas;
+	}
+
+	public int getFilas() {
+		return filas;
+	}
+
+	public int getColumnas() {
+		return columnas;
+	}
+
+	public int getBombas() {
+		return bombas;
+	}
 	
 	
 }
